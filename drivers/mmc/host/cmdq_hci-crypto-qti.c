cde2ea49df53596eaa1dcb4c0042f440e5d18603
Neeraj Soni <neersoni@codeaurora.org>
2020-12-21 14:31:20 +0530

    mmc: Explicitly enable mmc card clocks before programing crypto key
    Command queue driver has customized changes to work on msm-4.14 where
    pm_runtime_get_sync() enables the host clocks but customized code releases
    the clock after 100ms of inactivity to save power.
    With new FBE V2 the crypto keys are programmed in submit_bio() context
    where pm_runtime_get_sync() is called by KSM driver but crypto clock need
    to be kept on before programming the crypto key to ensure crypto registers
    are accesible.
    So export and call the mmc clock APIs to hold the mmc and crypto clocks
    while programming crypto keys.
    
    Test: repeated power on/off test.
    
    Change-Id: I6cae7febdfac053f4477ab34f4265b9f4f24606c
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>

--- drivers/mmc/host/cmdq_hci-crypto-qti.c
+++ drivers/mmc/host/cmdq_hci-crypto-qti.c
@@ -129,6 +129,8 @@ static int cmdq_crypto_qti_keyslot_program(struct keyslot_manager *ksm,
 	if (err)
 		pr_err("%s: failed with error %d\n", __func__, err);
 
+	mmc_host_clk_release(mmc_host);
+
 	return err;
 }
 
